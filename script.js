c5e94801e418576a1e8daba977b5178311fa5359
ThaphatTT <thaphat.meechaitana@outlook.com>
2023-11-09 18:25:08 +0700

    Edit Front V3

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-const 
+const getInputTitle = document.getElementById('inputTitle');
+const getButtonIncrease = document.getElementById('increase');
+const getTaskTodolist = document.getElementById('To-dolist')
