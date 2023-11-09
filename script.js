70434dc825e71303cb47071f0b5479e6e5621c3f
ThaphatTT <thaphat.meechaitana@outlook.com>
2023-11-09 18:40:43 +0700

    script backend

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-const getInputTitle = document.getElementById('inputTitle');
-const getButtonIncrease = document.getElementById('increase');
-const getTaskTodolist = document.getElementById('To-dolist')
+function Increase(){
+    const getInputTitle = document.getElementById('inputTitle');
+    const getButtonIncrease = document.getElementById('increase');
+    const getTaskTodolist = document.getElementById('To-doList')
+        getButtonIncrease.addEventListener('click',function(){
+            const Text = getInputTitle.value;
+            if(Text !=''){
+                addText(Text);
+                getInputTitle.value ='';
+            }
+        })
+        function addText(Text){
+            const li = document.createElement('li');
+            li.innerHTML = `
+            <span>${Text}</span>
+            <span class="delete-btn">❌</span>
+            `;
+            li.querySelector('.delete-btn').addEventListener('click', function() {
+                li.remove();
+            });
+            getTaskTodolist.appendChild(li);
+        }
+
+}
